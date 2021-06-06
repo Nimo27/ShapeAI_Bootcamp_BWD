efda75889d0acb5a4d9ec6e5a90ab26a9c339294
Nimo27 <71583609+Nimo27@users.noreply.github.com>
2021-06-06 11:34:34 +0530

    Add files via upload

---
+++ Note2.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note2() {
+  return (
+    <div className="note">
+      <h1> React.js</h1>
+      <p>
+        React. js is an open-source JavaScript library that is used for building
+        user interfaces specifically for single-page applications.
+      </p>
+    </div>
+  );
+}
+
+export default Note2;
---
+++ Note3.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note3() {
+  return (
+    <div className="note">
+      <h1>Why React.js is so popular?</h1>
+      <p>Because of its simplicity and flexibility.</p>
+    </div>
+  );
+}
+
+export default Note3;
